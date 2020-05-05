ad354eb40ea07f435c29c945604a9063aa3d61ae
Krzysztof 'RouNdeL' Zdulski <krzysztof.zdulski.stud@pw.edu.pl>
2020-05-05 12:45:42 +0200

    Optimize time shift detection, by automatically calculating shift bounds

--- src/main/kotlin/Combiner.kt
+++ src/main/kotlin/Combiner.kt
@@ -1,14 +1,42 @@
+import kotlin.math.min
+import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
 class Combiner(private val records: ArrayList<Record>) {
+    init {
+        if (records.isEmpty()) {
+            throw IllegalArgumentException("Records may not be empty")
+        }
+    }
+
     fun detectTimeShift(
         otherRecords: ArrayList<Record>,
-        startShift: Long = 3 * 60 * 60,
-        endShift: Long = startShift,
+        minOverlap: Float = 0.8F,
         maxStdDev: Double = 10_000.0
     ): Long? {
+        if (minOverlap > 1 || minOverlap < 0) {
+            throw IllegalArgumentException("minOverlap has to be in range 0-1")
+        }
+
+        if (otherRecords.isEmpty()) {
+            throw IllegalArgumentException("Records may not be empty")
+        }
+
+        val last0 = records.last().timestamp
+        val first0 = records.first().timestamp
+        val last1 = otherRecords.last().timestamp
+        val first1 = otherRecords.first().timestamp
+
+        val maxOffset: Int = ((1 - minOverlap) * min(
+            last0 - first0,
+            last1 - first1
+        )).roundToInt()
+
+        val startShift = first0 - first1 - maxOffset
+        val endShift = last0 - last1 + maxOffset
+
         val shiftMap = HashMap<Long, Double>()
-        var shift = -startShift
+        var shift = startShift
 
         while (shift <= endShift) {
             val stdDev = getStdDevForTimeShift(otherRecords, shift)
@@ -27,7 +55,7 @@ class Combiner(private val records: ArrayList<Record>) {
 
     private fun getStdDevForTimeShift(
         otherRecords: ArrayList<Record>,
-        shift: Long, minOverlap: Float = 0.8F
+        shift: Long
     ): Double {
         var i = 0
         var j = 0
@@ -50,10 +78,10 @@ class Combiner(private val records: ArrayList<Record>) {
             }
         }
 
-        if (recordCount < otherRecords.size * minOverlap) {
-            return Double.POSITIVE_INFINITY
+        return if (recordCount == 0) {
+            Double.POSITIVE_INFINITY
+        } else {
+            sqrt(devSum / recordCount)
         }
-
-        return sqrt(devSum / recordCount)
     }
 }
